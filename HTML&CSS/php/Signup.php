6e48701d7d084f0d4e6e6b17fe4d2fa5baa4dc90
wellxchen <xu411@purdue.edu>
2014-10-13 22:12:13 -0400

    backup added

--- HTML&CSS/php/Signup.php
+++ HTML&CSS/php/Signup.php
@@ -1,40 +1,47 @@
 <?php
-	
+
+	function remainInPage () {
+		
+		header("Location: signup.html");
+		die();
+	}
 	
 	$dbhandle = mysqli_connect("sql3.freemysqlhosting.net","sql353761","bR7%eL9*");
 	mysqli_query($dbhandle,"USE sql353761");
 
-	// check if the inputs are empty
 	if ((!empty($_POST['username'])) && (!empty($_POST['password'])) && (!empty($_POST['email']))) {
-	
-	
+		$username=$_POST['username'];
+		$email=$_POST['email'];
+		$password=$_POST['password'];
+		$address=$_POST['address'];
+
+		$result = mysqli_query($dbhandle, "SELECT * FROM signup WHERE Username=$username");
 		
-			$username=$_POST['username'];
-			$email=$_POST['email'];
-			$password=$_POST['password'];
-			$address=$_POST['address'];
+		if (!$result) {
+	
+			if(!(mysql_num_rows($result) > 0)) {
 
-
-			
-			$result = mysqli_query($dbhandle,"SELECT * FROM signup s WHERE s.Username='$username'");
-			$num_rows = mysqli_num_rows($result);
-
-			if($num_rows != 0) {
-		
-				echo "failed";
-				
-			}
-
-			else {
-
-				$SQLString = "INSERT INTO signup (Username,Email,Password,Address) 
+				$SQLString = "INSERT INTO signup (Username,Email,Password,Address)
 				VALUES( '$username','$email', '$password', '$address')";
 				mysqli_query($dbhandle, $SQLString);
-				header("Location: ../index.html");
-				die();
-			}
 		
+				header("Location: success.html");
+				die();
+	
+			}
+			else {
+				remainInPage ();
+			}
+		}
+	
+		else {
+			remainInPage ();
+		}
 	}
-
+		
+	else {
+		
+		remainInPage ();
+	}	
 	
 ?>
