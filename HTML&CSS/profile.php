b2ddcb1380fbf2dee1a533e71400f8e452b639cf
wellxchen <xu411@purdue.edu>
2014-10-14 22:38:29 -0400

    index.html edited, remove the weird bottom login fields. Profile.php edited

--- HTML&CSS/profile.php
+++ HTML&CSS/profile.php
@@ -2,37 +2,49 @@
 
 	$dbhandle = mysqli_connect("sql3.freemysqlhosting.net","sql353761","bR7%eL9*");
 	mysqli_query($dbhandle,"USE sql353761");
+	
+	$username = $_POST['username'];
 
 	if ((!empty($_POST['original_password'])) || (!empty($_POST['new_password'])) || (!empty($_POST['confirm_password']))) {
 		
-			
+			printErr ("Passwords incomplete!");
 	}
 	
 	else {
 	
 		if (!empty($_POST['email'])) {
 			
+			update("Email", $_POST['email']), $username);
+			
 		}
 		
 		if (!empty($_POST['firstname'])) {
-			
+		
+			update("Firstname", $_POST['firstname']), $username);
 		}
+		
 		if (!empty($_POST['lastname'])) {
-			
+		
+			update("Lastname", $_POST['lastname']), $username);
+
 		}
 		
 		if (!empty($_POST['gender'])) {
 			
+			update("Gender", $_POST['gender']), $username);
 		}
 		
 		/*phone number*/
 		
 		if (!empty($_POST['areacode'])) {
+		
 			
 		}
+		
 		if (!empty($_POST['prefix'])) {
 			
 		}
+		
 		if (!empty($_POST['linenumber'])) {
 			
 		}
@@ -64,7 +76,19 @@
 		if (!empty($_POST['zipcode'])) {
 			
 		}
+		
+		/*password*/
+		
+		if ((!empty($_POST['original_password'])) && (!empty($_POST['new_password'])) && (!empty($_POST['confirm_password']))) {
+			
+			
+		}
 	}
+	
+	header("Location: success.html");
+	die();
+	
+	/*query database and update data*/
 	
 	function update ($itemToBeUpdated, $updated_data, $username) {
 		
@@ -74,6 +98,20 @@
 
 	   mysqli_select_db('test_db');
 	   mysqli_query($dbhandle, $SQLstring);
-	  
+	}
+	
+	/*print error*/
+	
+	function printErr ($_err) {
+
+		
+		echo '<html>
+				<head>
+					<meta http-equiv="refresh" content="3;url=signup.html" />
+				</head>
+				<body>';
+    	echo "$_err";
+		die();
+	
 	}
 ?>
