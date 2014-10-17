c0ab637a439f2c1e5d553b7883f762e36483b117
zhan1061 <zhan1061@purdue.edu>
2014-10-16 21:20:32 -0400

    test
    test

--- HTML&CSS/profile.php
+++ HTML&CSS/profile.php
@@ -1,23 +1,23 @@
 <?php
 	
 	session_start();
-	$user = $_SESSION['loginuser'];
+	$username = $_SESSION['loginuser'];
 	
 	$dbhandle = mysqli_connect("sql3.freemysqlhosting.net","sql353761","bR7%eL9*");
 	//choose the database you want to use or you can ignore this line and do DATABASE.table inside the mysqli_query();
 	mysqli_query($dbhandle,"USE sql353761");
 
-	$SQLString = "SELECT * FROM account WHERE Username='$user'";
+	$SQLString = "SELECT * FROM account WHERE Username='$username'";
 	$result = mysqli_query($dbhandle, $SQLString);
 
 	while($row = mysqli_fetch_assoc($result)) {
-		$user['username']=$row['username'];
-		$user['firstname']=$row['username'];
-		$user['lastname']=$row['lastname'];
-		$user['email']=$row['email'];
-		$user['birthDay']=$row['birthDay'];
-		$user['address']=$row['address'];
-		$user['phone']=$row['phone'];
+		$username['username']=$row['username'];
+		$username['firstname']=$row['username'];
+		$username['lastname']=$row['lastname'];
+		$username['email']=$row['email'];
+		$username['birthDay']=$row['birthDay'];
+		$username['address']=$row['address'];
+		$username['phone']=$row['phone'];
 	}
 	
 	function printErr ($_err) {
--- HTML&CSS/test.php
+++ HTML&CSS/test.php
@@ -62,28 +62,41 @@
                             	<li> <a href="profile(editMode).html">#Edit Your Profile</a></li>
                             </ul>
   
-  <?php
+<?php
+	
 	session_start();
-	$user = $_SESSION['loginuser'];
+	$username = $_SESSION['loginuser'];
 	
 	$dbhandle = mysqli_connect("sql3.freemysqlhosting.net","sql353761","bR7%eL9*");
 	//choose the database you want to use or you can ignore this line and do DATABASE.table inside the mysqli_query();
 	mysqli_query($dbhandle,"USE sql353761");
 
-	$SQLString = "SELECT * FROM account WHERE Username='$user'";
+	$SQLString = "SELECT * FROM account WHERE Username='$username'";
 	$result = mysqli_query($dbhandle, $SQLString);
-	
+
 	while($row = mysqli_fetch_assoc($result)) {
-		$user['username']=$row['username'];
-		$user['firstname']=$row['username'];
-		$user['lastname']=$row['lastname'];
-		$user['email']=$row['email'];
-		$user['birthDay']=$row['birthDay'];
-		$user['address']=$row['address'];
-		$user['phone']=$row['phone'];
-		echo $user['email'];
+		$username['username']=$row['username'];
+		$username['firstname']=$row['username'];
+		$username['lastname']=$row['lastname'];
+		$username['email']=$row['email'];
+		$username['birthDay']=$row['birthDay'];
+		$username['address']=$row['address'];
+		$username['phone']=$row['phone'];
 	}
 	
+	function printErr ($_err) {
+
+		
+		echo '<html>
+				<head>
+					<meta http-equiv="refresh" content="3;url=signup.html" />
+				</head>
+				<body>';
+    	echo "$_err";
+		die();
+	
+	}
+
 ?>
 <!--
   <div id="right">
@@ -96,28 +109,28 @@
                 <tr>
                   <td width="20%" align="right" bgcolor="#FFFFFF">Username： </td>
                   <td width="80%" align="left" bgcolor="#FFFFFF">
-                  <input name="username" type="text" value="<?php echo $user['username']; ?>" class="formBorder" readonly/>
+                  <input name="username" type="text" value="<?php echo $username['username']; ?>" class="formBorder" readonly/>
                   </td>
                 </tr>
 				
 				 <tr>
                   <td width="20%" align="right" bgcolor="#FFFFFF">Email： </td>
                   <td width="80" align="left" bgcolor="#FFFFFF">
-                  <input name="email" type="text" value="<?php echo $user['email']; ?>" placeholder=""/ readonly>
+                  <input name="email" type="text" value="<?php echo $username['email']; ?>" placeholder=""/ readonly>
                   </td>
                 </tr>
                 
                 <tr>
                   <td width="20%" align="right" bgcolor="#FFFFFF">First Name： </td>
                   <td width="80%" align="left" bgcolor="#FFFFFF">
-                  <input name="first name" type="text" value="<?php echo $user['firstname']; ?>" class="formBorder" readonly/>
+                  <input name="first name" type="text" value="<?php echo $username['firstname']; ?>" class="formBorder" readonly/>
                   </td>
                 </tr>
                 
                 <tr>
                   <td width="20%" align="right" bgcolor="#FFFFFF">Last name： </td>
                   <td width="80%" align="left" bgcolor="#FFFFFF">
-                  <input name="last name" type="text" value="<?php echo $user['lastname']; ?>" class="formBorder" readonly/>
+                  <input name="last name" type="text" value="<?php echo $username['lastname']; ?>" class="formBorder" readonly/>
                   </td>
                 </tr>
               
@@ -127,7 +140,7 @@
                   <td width="72%" align="left" bgcolor="#FFFFFF">
                   <div class="row uniform">
                   <div class="6u">
-				  	<input name="birthday" type="text" value="<?php echo $user['birthDay']; ?>" class="formBorder" readonly/>
+				  	<input name="birthday" type="text" value="<?php echo $username['birthDay']; ?>" class="formBorder" readonly/>
                   </div>
                   </div>
                   </td>
@@ -135,14 +148,14 @@
                 <tr>
                   <td width="20%" align="right" bgcolor="#FFFFFF">Phone number： </td>
                   <td width="80%" align="left" bgcolor="#FFFFFF">
-                  <input name="phone" type="text" value="<?php echo $user['phone']; ?>" class="formBorder" readonly/>
+                  <input name="phone" type="text" value="<?php echo $username['phone']; ?>" class="formBorder" readonly/>
                   </td>
                 </tr>
                 
                 <tr>
                   <td width="20%" align="right" bgcolor="#FFFFFF">Mailing address： </td>
                   <td width="80%" align="left" bgcolor="#FFFFFF">
-                  <input name="address" type="text" value="<?php echo $user['address']; ?>" class="formBorder" readonly/>
+                  <input name="address" type="text" value="<?php echo $username['address']; ?>" class="formBorder" readonly/>
                   </td>
                 </tr>                <tr>
                   <td width="20%" align="right" bgcolor="#FFFFFF">Reserved Ticktets: </td>
