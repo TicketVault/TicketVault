e314aabbc262e646fe18d12d90ed554a000b0a0b
Hanxun Huang <Curtis@Hanxuns-MacBook-Pro.local>
2014-10-16 20:25:09 -0400

    Loginerror fixed

--- HTML&CSS/login.php
+++ HTML&CSS/login.php
@@ -3,13 +3,10 @@
 	Alpha by HTML5 UP
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-	
 -->
-
-
 <html>
 	<head>
-		<title>TicketVault</title>
+		<title>Signup - TicketVault</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
@@ -20,7 +17,14 @@
 		<script src="js/skel.min.js"></script>
 		<script src="js/skel-layers.min.js"></script>
 		<script src="js/init.js"></script>
-		<script src="js/dropdown.js"></script>
+        <script type="text/javascript">
+			function ValidateForm(frm) {
+				if (frm.username.value == "") {alert('Username is required.');frm.username.focus();return false;}
+				if (frm.pwd.value == "") {alert('Password is required.');frm.pwd.focus();return false;}
+			return true; 
+		}
+		</script>
+
 		<noscript>
 			<link rel="stylesheet" href="css/skel.css" />
 			<link rel="stylesheet" href="css/style.css" />
@@ -28,13 +32,11 @@
 		</noscript>
 		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
 	</head>
-	
-	
-	
-	<body class="landing">
+	<body>
+
 		<!-- Header -->
-			<header id="header" class="alt">
-				<h1><a href="http://ticketvault.cu.cc">TicketVault</a></h1>
+			<header id="header">
+				<h1><a href="index.html">TicketVault</a></h1>
 				<?php
 					session_start();
 					if(isset($_SESSION['loginuser'])){
@@ -55,64 +57,69 @@
 						echo "<li><a href=".""." >Manage Event</a></li>";
 						echo "<li><a href=".""." >Upcoming Event</a></li>";
 						echo "<li><a href="."signup.html"." class="."button".">Sign Up</a></li>";
-						echo "<li><a href="."login.php"." class="."button".">login</a></li>";
+						echo "<li><a href="."login.html"." class="."button".">login</a></li>";
 						echo "</ul>";
 						echo "</nav>";
 					}
 				?>
-				</header>
-
 				<!--
-					<nav id="nav">
+				<nav id="nav">
 					<ul>
 						<li><a href="index.html" >Home</a></li>
 						<li><a href="index.html" >Manage Event</a></li>
 						<li><a href="index.html" >Upcoming Event</a></li>
 						<li><a href="signup.html" class="button">Sign Up</a></li>
+						<li><a href="profile.html" >Profile</a></li>
 						<li><a href="login.html" class="button">Login</a></li>
 					</ul>
-				</nav>
+						</li>
+					</ul>
+				</nav>-->
 			</header>
-			-->
-			
-			
 
-			
-		<!-- banner -->
-			<section id="banner">
-				
-				<p>Search for Event or Organazation</p>				
-				<form>
-					<div class="row uniform half collapse-at-2">
-						<div class="6u">
-							<input type="text"  placeholder="Event Name" />
+		<!-- Main -->
+			<section id="main" class="container small">
+				<header>
+					<h2>Login</h2>
+					
+				</header>
+				<div class="box">
+					<form method="post" action="php/Login.php" onsubmit="return ValidateForm(this);">
+						<div class="row uniform half">
+							<div class="12u">
+								<input type="text" name="username" id="username" value="" placeholder="User Name" />
+							</div>
 						</div>
-						<div class="3u">
-							<input type="submit" value="Search" class="fit" />
+						<div class="row uniform half">
+							<div class="12u">
+								<input type="password" name="password" id="pwd" value="" placeholder="Password" />
+								<h5>  </h5>
+							</div>
+							</div>
+
+						<div class="row uniform">
+							<div class="12u">
+								<ul class="actions align-center">
+									
+									<input type="submit" name = "submit" id="button" value="submit"/>
+								</ul>
+							</div>
 						</div>
-					</div>
-				</form>
+					</form>
+				</div>
 			</section>
 			
 		<!-- Footer -->
 			<footer id="footer">
 				<ul class="icons">
-					<li><a href="https://github.com/TicketVault/TicketVault" class="icon fa-github"><span class="label">Github</span></a></li>
+				<li><a href="https://github.com/TicketVault/TicketVault" class="icon fa-github"><span class="label">Github</span></a></li>
 					<li><a href="https://www.cs.purdue.edu/homes/bxd/307/" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
 
-				<!--
-					<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-					<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-					<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-					<li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
-					<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
-					<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
-					-->
 				</ul>
 				<ul class="copyright">
-					<li>&copy; CS307 Fall2014 Team8</li>
+					<li>&copy; CS307 Fall2014 Team8.</li>
 				</ul>
 			</footer>
-						
+
 	</body>
 </html>
---
+++ HTML&CSS/php/Logout.php
@@ -0,0 +1,9 @@
+<?php
+	//this is php commrnt
+	//database connection
+	session_start();
+	session_unset();
+	session_destroy();
+	header("location:http://ticketvault.cu.cc");
+	exit();
+?>
