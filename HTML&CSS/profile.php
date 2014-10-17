85c2ac9445f4dfbdc0f515b704d177cae2925775
zhan1061 <zhan1061@purdue.edu>
2014-10-16 20:36:39 -0400

    test
    tesst

--- HTML&CSS/profile.php
+++ HTML&CSS/profile.php
@@ -1,25 +1,16 @@
 <?php
 	
-	/*Author: Chengkang Xu*/
-	/*Handle interconnection between server and profile(editMode) page*/
+	session_start();
+	$user = $_SESSION['loginuser'];
 	
 	$dbhandle = mysqli_connect("sql3.freemysqlhosting.net","sql353761","bR7%eL9*");
 	//choose the database you want to use or you can ignore this line and do DATABASE.table inside the mysqli_query();
 	mysqli_query($dbhandle,"USE sql353761");
 
-	$username=$_POST["username"];
-	$password=$_POST["password"];
-
-	
-	$SQLString = "SELECT * FROM account WHERE Username='$username'";
+	$SQLString = "SELECT * FROM account WHERE Username='$user'";
 	$result = mysqli_query($dbhandle, $SQLString);
-	$count=mysqli_num_rows($result);
-	session_start();
-	$_SESSION['loginuser']=$username;
-	while($row = mysqli_fetch_array($result)) {
-  		echo "<tr>";
-  		echo "<td>" . $row['username'] . "</td>";
-  		echo "<td>" . $row['lastName'] . "</td>";
-  		echo "</tr>";
-	}
+	$row = mysql_fetch_assoc($sql);
+		$user['username']=$row['username'];
+		echo $row['username'];
+	
 ?>
