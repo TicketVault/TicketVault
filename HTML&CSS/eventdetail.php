07561db13a321fc2284735cbea039497743d10f5
xu411 <xu411@purdue.edu>
2014-11-13 17:39:27 -0500

    eventdetail improved

--- HTML&CSS/eventdetail.php
+++ HTML&CSS/eventdetail.php
@@ -65,13 +65,6 @@
   
 <?php	
 	
-	session_start();
-	$dbhandle = database_connect();
-	$eventname = $_GET['event_name'];
-	$SQLString = "SELECT * FROM event WHERE Eventname = '$eventname'";
-	$result = mysqli_query($dbhandle, $SQLString);
-	$row = mysqli_fetch_assoc($result);
-	
 	
 	if (isset($_SESSION['loginuser'])) {
 	
