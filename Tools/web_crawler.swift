40c006518ec71adc0e7cc7b6902de04eb2bda710
HanxunHuangLemonBear <curtishuang@foxmail.com>
2014-11-17 19:14:27 -0500

    Web_crawler for boiler link connection setup

---
+++ Tools/web_crawler.swift
@@ -0,0 +1,26 @@
+//
+//  test.swift
+//  
+//
+//  Created by HuangHanxun on 14/11/17.
+//
+//
+
+import Foundation
+//Setup baseurl
+var i = 0;
+var baseurl = "https://boilerlink.purdue.edu/Organizations?SearchType=None&SelectedCategoryId=0&CurrentPage="
+var index = 0
+var targeturl:String
+var key = "target="+"\u{0022}"+"_self"+"\u{0022}"+">"
+//println(key)
+for(i=1;i<103;i++){
+    index = i
+    targeturl = baseurl + String(index)
+    println(targeturl)
+    let targeturl_NSURL = NSURL(string:targeturl);
+    let data = NSData(contentsOfURL: targeturl_NSURL!, options: nil, error: nil)
+    var datastring_NSString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+    //println(datastring_NSString)
+}
+
