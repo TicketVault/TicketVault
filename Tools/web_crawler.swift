71007a2ea1493925964768ebd916cfaa7b9020e1
HanxunHuangLemonBear <curtishuang@foxmail.com>
2014-11-18 20:00:25 -0500

    RegularExpression get all the orz name
    Now can output all the raw data of organization name

--- Tools/web_crawler.swift
+++ Tools/web_crawler.swift
@@ -1,5 +1,5 @@
 //
-//  test.swift
+//  web_crawler
 //  
 //
 //  Created by HuangHanxun on 14/11/17.
@@ -13,6 +13,11 @@ var baseurl = "https://boilerlink.purdue.edu/Organizations?SearchType=None&Selec
 var index = 0
 var targeturl:String
 var key = "target="+"\u{0022}"+"_self"+"\u{0022}"+">"
+let myRegex = "[t][a][r][g][e][t][=][\u{0022}][_][s][e][l][f][\u{0022}][>](.){1,}[>]"
+var key_length = countElements(key)
+var j = 0;
+var m = 0;
+var error:NSError?
 //println(key)
 for(i=1;i<103;i++){
     index = i
@@ -22,5 +27,17 @@ for(i=1;i<103;i++){
     let data = NSData(contentsOfURL: targeturl_NSURL!, options: nil, error: nil)
     var datastring_NSString = NSString(data: data!, encoding: NSUTF8StringEncoding)
     //println(datastring_NSString)
+    var datastring = String(datastring_NSString!)
+    var data_string_length = countElements(datastring)
+    while var match = datastring.rangeOfString(myRegex, options: .RegularExpressionSearch){
+        println(datastring[match])
+        datastring.replaceRange(match, with: " ")
+    }
 }
-
+/*
+let myStringToBeMatched = "ThisIsMyString ThisIsMyString"
+let myRegex = "ing"
+if var match = myStringToBeMatched.rangeOfString(myRegex, options: .RegularExpressionSearch){
+    println("\(myStringToBeMatched) is matching!")
+    println(myStringToBeMatched[match])
+}*/
