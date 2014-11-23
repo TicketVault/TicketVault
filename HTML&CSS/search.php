ce7937d89fccb59cd96424e41e706a35fbd88755
YiJieWu <yijiewuboilerup@gmail.com>
2014-11-23 09:44:22 -0500

    add db connection

--- HTML&CSS/search.php
+++ HTML&CSS/search.php
@@ -1,4 +1,11 @@
-<php?
+<?php
+
+$dbhandle = mysqli_connect("mysql.hostinger.co.uk","u907917272_cs307","CS307TEAM8","u907917272_table");
+		if (mysqli_connect_errno())
+		{
+  			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  		}
+  		return $dbhandle; 
 
 
 
