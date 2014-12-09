a3039a1a06227d9fda76ff6c2fc06f2adb7a57ed
HanxunHuangLemonBear <curtishuang@foxmail.com>
2014-12-09 15:56:34 -0500

    update

--- HTML&CSS/php/Base.php
+++ HTML&CSS/php/Base.php
@@ -118,13 +118,11 @@
 		// To send HTML mail, the Content-type header must be set
 		$headers  = 'MIME-Version: 1.0' . "\r\n";
 		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
 		// Additional headers
-	$headers .= 'From: Ticket Vault <support@ticketvault.cu.cc>' . "\r\n";
-	$headers .= 'Cc: '. "\r\n";
-	$headers .= 'Bcc: ' . "\r\n";
-	
-	// Mail it
-	mail($to, $subject, $message, $headers);
+		$headers .= 'From: Ticket Vault <support@ticketvault.cu.cc>' . "\r\n";
+		$headers .= 'Cc: '. "\r\n";
+		$headers .= 'Bcc: ' . "\r\n";
+			// Mail it
+		mail($to, $subject, $message, $headers);
 	}
 ?>
